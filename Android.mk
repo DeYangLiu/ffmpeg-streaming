d178ffd85f98909da65f01a544e7cd094995c24b
DeYangLiu <liu_de_yang@qq.com>
2018-04-24 14:35:18 +0800

    fix last commit.

--- Android.mk
+++ Android.mk
@@ -2,18 +2,16 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := file_server
-<<<<<<< HEAD
 LOCAL_CFLAGS := -DFFMPEG_SRC=0 -DPLUGIN_ZLIB=0
 LOCAL_C_INCLUDES += #
 
 LOCAL_SRC_FILES := ffserver.c compact.c avstring.c
 LOCAL_LD_FLAGS := -static
-=======
 LOCAL_CFLAGS := -DFFMPEG_SRC=0 -DPLUGIN_ZLIB=0 
 LOCAL_C_INCLUDES += #
 
 LOCAL_SRC_FILES := ffserver.c compact.c avstring.c
 #LOCAL_LDFLAGS := -static
->>>>>>> d98d3f2f40eb53ba0c8c2a894eda98a62fd7107c
+
 
 include $(BUILD_EXECUTABLE)
